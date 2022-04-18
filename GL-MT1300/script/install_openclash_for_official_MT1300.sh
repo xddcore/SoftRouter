da4793ef7f24b7cfc8881039587a8d26f752b080
Chengsen Dong <1034029664@qq.com>
2022-04-18 19:00:00 +0100

    Update install_openclash_for_official_MT1300.sh

--- GL-MT1300/script/install_openclash_for_official_MT1300.sh
+++ GL-MT1300/script/install_openclash_for_official_MT1300.sh
@@ -1,6 +1,6 @@
 ############### Install OpenClash###############
 # Author: xddcore
-# Date:03/09/2021
+# Date:18/04/2022
 # 1034029664@qq.com
 #Used in GL.iNet official firmware
 opkg update
@@ -11,16 +11,16 @@ opkg install coreutils-nohup bash iptables dnsmasq-full curl jsonfilter ca-certi
 # install libcap-bin, libcap(opkg install version is too early)
 #Source: https://downloads.openwrt.org/snapshots/packages/mipsel_24kc/base/
 cd /tmp
-wget --no-check-certificate https://downloads.openwrt.org/snapshots/packages/mipsel_24kc/base/libcap_2.51-1_mipsel_24kc.ipk
+wget --no-check-certificate https://downloads.openwrt.org/snapshots/packages/mipsel_24kc/base/libcap_2.63-1_mipsel_24kc.ipk
 opkg install /tmp/libcap_*.ipk
-wget --no-check-certificate https://downloads.openwrt.org/snapshots/packages/mipsel_24kc/base/libcap-bin_2.51-1_mipsel_24kc.ipk
+wget --no-check-certificate https://downloads.openwrt.org/snapshots/packages/mipsel_24kc/base/libcap-bin_2.63-1_mipsel_24kc.ipk
 opkg install /tmp/libcap-bin_*.ipk
 rm -f /tmp/libcap_*.ipk
 rm -f /tmp/libcap-bin_*.ipk
 
 # install luci-app-openclash, will try to get latest version later
 #https://github.com/vernesong/OpenClash/releases
-wget --no-check-certificate https://github.com/vernesong/OpenClash/releases/download/v0.43.01-beta/luci-app-openclash_0.43.01-beta_all.ipk
+wget --no-check-certificate https://github.com/vernesong/OpenClash/releases/download/v0.45.02-beta/luci-app-openclash_0.45.02-beta_all.ipk
 opkg install /tmp/luci-app-openclash_*.ipk
 rm -f /tmp/luci-app-openclash_*.ipk
 
@@ -32,7 +32,7 @@ tar zxvf clash-linux-mipsle-*.tar.gz
 rm -f clash-linux-mipsle-*.gz
 
 echo Author: xddcore
-echo Date:03/09/2021
+echo Date:18/04/2022
 echo 1034029664@qq.com
 echo Finish Install OpenClash,Enjoy it!
 
